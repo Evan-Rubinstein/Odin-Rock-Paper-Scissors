b6107d478ffb7c72d49a8a7a3708ff05416561da
Evan-Rubinstein <evanr50it@gmail.com>
2022-09-01 18:43:36 -0400

    Program game logic

--- javascript.js
+++ javascript.js
@@ -9,8 +9,33 @@ function getComputerChoice() {
 }
 // Function called playRound takes playerChoice and computerChoice as parameters, and declares the winner of the round.
 function playRound(computerChoice, playerChoice){
-    if (computerChoice == playerChoice) {
+    if (computerChoice === playerChoice) { //Logic for if the computer and player pick the same element
         console.log("You have both picked "+computerChoice+", this round is a tie")
+    }
+    else if (computerChoice === "rock") { //Logic for if the computerChoice is rock. 
+        if (playerChoice === "scissors") {
+            console.log("Computer Wins!")
+        }
+        else if (playerChoice === "paper") {
+            console.log("Player Wins!")
+        }
+        else {
+            console.log("Something ain't right here...")
+        }
+    }
+    else if (computerChoice === "paper") { // Logic for if the computerChoice is scissors
+        if (playerChoice === "rock") {
+            console.log("Computer Wins!")
+        }
+        else if (playerChoice == "scissors") {
+            console.log("Player Wins!")
+        }
+        else {
+            console.log("Something ain't right here 2")
+        }
+    }
+    else {
+        console.log("Something ain't right here 3")
     }
 }
 
