1caf1f0cb385cb4f0b970e8f970af3e028f6c98c
Evan-Rubinstein <evanr50it@gmail.com>
2022-08-31 23:13:32 -0400

    Fix getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -3,10 +3,17 @@
 
 // Function called getComputerChoice will randomly return either rock, paper or scissors
 function getComputerChoice() {
-    const choices = array['rock','paper', 'scissors']
-    const pick = Math.floor(Math.random() * choices.length)
-    return pick;
+    const choices = ['rock','paper', 'scissors']
+    const pick = Math.floor(Math.random() * 3)
+    return choices[pick];
 }
 // Function called playRound takes playerChoice and computerChoice as parameters, and declares the winner of the round.
 
 // Function called game runs playRound function five times, keeps track of score and declares a winner
+
+
+
+
+
+// Testing code
+console.log(getComputerChoice())
